f2a7cb8b5a995278c8a104cc02e17b98722043ba
Neilda <neilda@neildapacquing.com>
2015-01-29 17:21:53 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instalearn::Application.config.secret_key_base = '13026379b3939ddd7f46b5a1c7c4a8fa08baf8e15beb660237286b1595a5bea0d5e96cda2ec2744ecaf95694bbc087ac34d4a8e1846c43638ea29fac0212dd4e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Instalearn::Application.config.session_store :cookie_store, key: '_instalearn_session'
